69b79937f205bac2b9137d371340bb5f7b460ebc
dhunanyan <davit.hunanyan@me.com>
2022-04-07 03:57:20 +0200

    IP Address access

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello to Memories API");
+  res.send("Hello to Memories API ");
 });
 
 // https://www.mongodb.com/cloud/atlas
